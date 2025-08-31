c79e2701a6ef8f0cd09aeca624620f04a0d35295
ShunheWang <maisonwangsh@gmail.com>
2025-08-31 16:05:52 +0800

    feature: init() ai项目 ;

---
+++ src/main/java/com/maison/maisonaiagent/MaisonAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.maison.maisonaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaisonAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaisonAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maison/maisonaiagent/MaisonAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maison.maisonaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisonAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
